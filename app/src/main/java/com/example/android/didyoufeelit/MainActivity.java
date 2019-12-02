3523bd0b991c571a3d5f6b6a644798fd67f3c3c2
Foncham <theo.foncham@gmail.com>
2019-12-02 08:18:14 +0100

    created inner asynctask class to fetch background information hence app does not crash

--- app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
+++ app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -72,18 +72,29 @@ public class MainActivity extends AppCompatActivity {
          * It is NOT okay to update the UI from a background thread, so we just return an
          * {@link Event} object as the result.
          */
-
-        @Override
         protected Event doInBackground(String... urls) {
+            // Don't perform the request if there are no URLs, or the first URL is null.
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
 
-            // Perform the HTTP request for earthquake data and process the response.
             Event result = Utils.fetchEarthquakeData(urls[0]);
-
             return result;
         }
 
-        @Override
+        /**
+         * This method is invoked on the main UI thread after the background work has been
+         * completed.
+         *
+         * It IS okay to modify the UI within this method. We take the {@link Event} object
+         * (which was returned from the doInBackground() method) and update the views on the screen.
+         */
         protected void onPostExecute(Event result) {
+            // If there is no result, do nothing.
+            if (result == null) {
+                return;
+            }
+
             updateUi(result);
         }
     }
